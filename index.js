98ae2961bab465f91977fd048ff2aba6c816e2cc
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-09-25 13:01:59 -0700

    my frist node project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("thank you for colling me ;fdlk;s");
+}) 
+
+
+app.listen(4000, () => console.log('l;aklsjdf a;lskjfkasjlfh'));
