899bd00ba9e55369f6d4234566809891a699def8
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-09-27 15:16:08 -0700

    added

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send("thank you for colling me ;fdlk;s");
-}) 
+const users = ["asad", 'mahiya', 'mohim', 'amir', 'anaer', 'banu'];
+
+ 
 
 
-app.listen(4000, () => console.log('l;aklsjdf a;lskjfkasjlfh'));
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+
+})
+
+//post
+app.post('/addUser', (req, res) => {
+    // save to database
+    const user = req.body;
+    user.id =55;
+    res.send(user);
+})
+
+app.listen(3000, () => console.log('node is very easy'));
